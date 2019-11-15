650e7f2391c2a965f7dea453cd7c4c66e603c101
fenglin098 <feng.lin098@gmail.com>
2019-11-15 15:11:41 -0500

    first commit

---
+++ src/main/java/com/example/demo/BootCampBlogApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCampBlogApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BootCampBlogApplication.class, args);
+    }
+
+}
