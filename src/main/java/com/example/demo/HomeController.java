4f5e51f72852a6731f3876137a7a56ec48ab531e
feng.lin098 <feng.lin098@gmail.com>
2019-11-17 19:10:49 -0500

    draft

--- src/main/java/com/example/demo/HomeController.java
+++ src/main/java/com/example/demo/HomeController.java
@@ -6,8 +6,18 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 
 public class HomeController {
-    @GetMapping("/home")
+    @GetMapping("/")
     public String home(){
         return "home";
+    }
+
+    @GetMapping("/about")
+    public String about(){
+        return "about";
+    }
+
+    @GetMapping("/bootcamp")
+    public String bootcamp(){
+        return "bootcamp";
     }
 }
